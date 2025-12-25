21ce98b4cc73d0c107bc31054ba29c307bbb591b
sachin <91665868+kitjsr@users.noreply.github.com>
2025-12-25 09:52:00 +0530

    update

--- src/index.js
+++ src/index.js
@@ -7,14 +7,17 @@ import { BrowserRouter } from 'react-router';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import { Provider } from "react-redux";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </React.StrictMode>
   </BrowserRouter>
 );
 
